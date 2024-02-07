f93443d67896c8766ca1eb675052d1263bb673fa
devopsinf-pichilemu <136821917+devopsinf-pichilemu@users.noreply.github.com>
2024-02-07 09:19:05 -0300

    Seed: Beneficiarios de ejemplo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Beneficiario;
 use App\Models\Tipo;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -25,10 +26,16 @@ class DatabaseSeeder extends Seeder
 
         /* Crear tipos de prueba */
         Tipo::insert([
-            ['nombre' => 'Cirugía', 'descripcion' => 'Aporte de pago total o parcial al pago de una cirugía.'],
-            ['nombre' => 'Ópticos', 'descripcion' => 'Aporte de pago total o parcial a la compra de lentes ópticos.'],
-            ['nombre' => 'Combustible', 'descripcion' => 'Aporte de pago total o parcial a la compra de combustible para movilización.'],
-            ['nombre' => 'Balón de gas', 'descripcion' => 'Entrega de vale para canje de balón de gas.'],
+            ['name' => 'Cirugía', 'description' => 'Aporte de pago total o parcial al pago de una cirugía.'],
+            ['name' => 'Ópticos', 'description' => 'Aporte de pago total o parcial a la compra de lentes ópticos.'],
+            ['name' => 'Combustible', 'description' => 'Aporte de pago total o parcial a la compra de combustible para movilización.'],
+            ['name' => 'Balón de gas', 'description' => 'Entrega de vale para canje de balón de gas.'],
+        ]);
+
+        /* Crear beneficiarios de prueba */
+        Beneficiario::insert([
+            ['id' => 1, 'rut_num' => 16678978, 'rut_vd' => '8', 'names' => 'Marco Ariel', 'lastname_1' => 'González', 'lastname_2' => 'Luengo', 'address' => 'Ángel Gaete 365', 'phone' => 998827027, 'nationality' => 'CL', 'annotations' => 'Beneficiario de prueba. **No molestar.**'],
+            ['id' => 2, 'rut_num' => 10878290, 'rut_vd' => '0', 'names' => 'Marta Noemí', 'lastname_1' => 'Luengo', 'lastname_2' => 'Zambrano', 'address' => 'Manuel Thompson 40', 'phone' => 976409171, 'nationality' => 'CL', 'annotations' => 'Beneficiaria de prueba. **Dejar tranquila.**'],
         ]);
     }
 }
