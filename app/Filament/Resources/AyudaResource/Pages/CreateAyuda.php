55f0f14cffc31ce38f155a7def904cecffe8cd84
devopsinf-pichilemu <136821917+devopsinf-pichilemu@users.noreply.github.com>
2024-02-07 11:24:22 -0300

    Ayuda: Redirigir al listado después de crear o editar.

--- app/Filament/Resources/AyudaResource/Pages/CreateAyuda.php
+++ app/Filament/Resources/AyudaResource/Pages/CreateAyuda.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\AyudaResource\Pages;
 
 use App\Filament\Resources\AyudaResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateAyuda extends CreateRecord
@@ -11,4 +10,9 @@ class CreateAyuda extends CreateRecord
     protected static string $resource = AyudaResource::class;
 
     protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
