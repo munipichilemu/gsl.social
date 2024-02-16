171dd51c9b0b1a1a2b327970ee8c1323ee142a4b
devopsinf-pichilemu <136821917+devopsinf-pichilemu@users.noreply.github.com>
2024-02-16 11:42:04 -0300

    Beneficiario: Crear Factory para generar registros de prueba.

--- database/factories/BeneficiarioFactory.php
+++ database/factories/BeneficiarioFactory.php
@@ -19,16 +19,17 @@ class BeneficiarioFactory extends Factory
     public function definition(): array
     {
         $country = new Country('list');
+        $gender = fake()->randomElement(['female', 'male']);
 
         return [
             'rut' => fake()->rut(),
-            'names' => fake()->firstName . ' ' . fake()->firstName,                // 2 nombres. TODO: Fijar el género antes de crear los nombres, y hacer un random entre 1 y 3 nombres.
+            'names' => fake()->firstName($gender).' '.fake()->firstName($gender),  // 2 nombres.
             'lastname_1' => fake()->lastName(),
             'lastname_2' => fake()->lastName(),
             'address' => explode(',', fake()->address())[0],            // Nos quedamos con el nombre de la calle y el número de casa.
             'phone' => fake()->numberBetween(111111111, 999999999),     // Esto nos genera toda la parte que no involucra el prefijo universal.
             'nationality' => fake()->randomKey($country->getList()),               // Obtenemos la lista de países del paquete del listado de países, pero solo usamos sus keys.
-            'annotations' => fake()->text()
+            'annotations' => fake()->text(),
         ];
     }
 }
